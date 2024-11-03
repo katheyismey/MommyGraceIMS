9a8f3fcb3eda513fab86937e4bc66ddd13e1dd01
Donald Sombrio <sombriodonaldio@gmail.com>
2024-11-03 18:18:46 +0800

    Delete InventoryManagement directory

--- InventoryManagement/AppInventoryManagement/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppinventorymanagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppInventoryManagement'
--- InventoryManagement/AppInventoryManagement/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductListView, ProductCreateView
-
-urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product_list'),  # URL for listing products
-    path('products/add/', ProductCreateView.as_view(), name='product_add'),  # URL for adding a new product
-]
